44a23afd4778cc67b41bcad775230a227d6a80d1
Antonio Aguilar <antonio.aguilar@helloiconic.com>
2023-04-12 15:45:39 -0600

    PokemonAPI - clase039.0

--- clase038-041/src/index.ts
+++ clase038-041/src/index.ts
@@ -1,26 +1,34 @@
 import express from "express";
-// import * as cookieParser from "cookie-parser";
 import * as cors from "cors";
+import connectDatabase from "./database";
+import { PORT } from "../config";
+import { logger } from "./logger";
+// Import routers
 import pokemonRouter from "./routes/pokemon.router";
-
-import connectDatabase from "./database"
-connectDatabase();
+import pokemonRepository from "./repository/pokemon.repository";
 
 const app = express();
-import { PORT } from "../config";
+app.set("view engine", "pug");
+app.set("views", __dirname + "/../views/");
 
+// Connect to database
+connectDatabase();
+
+// app.use(cookieParser(SECRET));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser(SECRET));
 app.use(cors());
+app.use(express.static(__dirname + "/../public"));
 
 // Routes
-import { logger } from "./logger";
-
-app.get("/", (req, res) => res.json({ time: Date.now() }));
-
 app.use("/api/pokemon", pokemonRouter);
+
+app.get("/", (req, res) => {res.json({time: Date.now()})});
 
 const server = app.listen(PORT, () => {
   logger.info(`🔋 Server running on port::${PORT}`);
+});
+
+server.on("error", (error) => {
+  logger.error(`🔥 Server error: ${error.message}`);
 });
